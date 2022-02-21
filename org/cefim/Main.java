b8a191f18dacdd2a939cde1578f838779cc9649c
patrick <patrick@brouille.net>
2022-02-21 15:20:14 +0100

    (feature) Main class + gitignore

---
+++ org/cefim/Main.java
@@ -0,0 +1,17 @@
+package org.cefim;
+
+/**
+ * Ecrire un programme qui retourne les entiers de 1 à 100
+ * A prendre en compte :
+ * Pour les multiples de 3, remplacer le nombre pas "Fizz"
+ * Pour les multiples de 5, remplacer le nombre pas "Buzz"
+ * Pour les multiples de 15, remplacer le nombre pas "FizzBuzz"
+ *
+ * Exemple : "12Fizz4BuzzFizz78FizzBuzz...FizzBuzz... Buzz"
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+    }
+}
