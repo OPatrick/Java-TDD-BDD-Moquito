f418bf2ddcad05f487b8ba10f079290fc260a640
patrick <patrick@brouille.net>
2022-02-21 15:53:56 +0100

    (feature) MainTest class + gitignore

---
+++ test/org/cefim/MainTest.java
@@ -0,0 +1,35 @@
+package test.org.cefim;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/** 
+* Main Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>févr. 21, 2022</pre> 
+* @version 1.0 
+*/ 
+public class MainTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: main(String[] args) 
+* 
+*/ 
+@Test
+public void testMain() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
